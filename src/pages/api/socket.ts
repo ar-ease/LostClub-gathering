fc2e723fe5a15281aef7caadfa5f7ec1aa2cbc9e
ar-ease <arghya0017@gmail.com>
2025-06-28 03:45:46 +0530

    prod 2

--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -1,7 +1,11 @@
+import { NextApiRequest } from "next";
 import { NextApiResponseServerIO } from "@/lib/socket";
 import { initializeSocket } from "@/lib/socket";
 
-export default function handler(_req, res: NextApiResponseServerIO) {
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponseServerIO,
+) {
   if (!res.socket.server.io) {
     console.log("Setting up socket server...");
     const io = initializeSocket(res.socket.server);
